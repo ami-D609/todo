9fe603a27f506b90c22855a0f46d1470213567d7
ami-D609 <amithal.d6009@gmail.com>
2026-05-15 14:36:24 +0530

    Modify reminder alert and sound playback logic

--- script.js
+++ script.js
@@ -233,11 +233,12 @@ document.addEventListener('DOMContentLoaded', () => {
    {
     setTimeout(()=>{
         
-        alert(`Reminder:${todoText}`);
-      playClickSound(5000,1);
+        
+      playClickSound(5000,3);
       playClickSound(500,1);
           playClickSound(1000,0.5);
       playClickSound(300,0.05);
+        alert(`Reminder:${todoText}`);
         todoList.removeChild(document.querySelector(`.todo-item`));
         saveTodos();}
 ,timer)
